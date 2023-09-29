73023f4fa85137a0087785702ff29b8e9e93cbf7
MARICHELLE BISUNA <marichellebisuna@MARICHELLEs-MacBook-Air.local>
2023-09-29 14:25:03 +1000

    Initial modal UI created and Uploadthing setup done

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from 'next/navigation';
 import { db } from '@/lib/db';
 import { initialProfile } from '@/lib/initial-profile';
+import { InitialModal } from '@/components/modals/initial-modals';
 
 const SetupPage = async () => {
   const profile = await initialProfile();
@@ -17,7 +18,7 @@ const SetupPage = async () => {
   if (server) {
     return redirect(`/servers/${server.id}`);
   }
-  return <div>Create a Server</div>;
+  return <InitialModal />;
 };
 
 export default SetupPage;
